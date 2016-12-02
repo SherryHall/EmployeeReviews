2576017dc96d3a751b7a9f0e97efe554be8baab8
SherryHall <slhall222@gmail.com>
2016-12-02 00:34:54 -0500

    Add services classes, start layout

---
+++ EmployeeReviews.Services/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeReviews.Services
+{
+	class Employee
+	{
+		public Guid EmpId { get; set; } = Guid.NewGuid();
+		public string EmpName { get; set; }
+		public string EmpEmail { get; set; }
+		public double EmpSalary { get; set; }
+
+		public Reviews EmpReview { get; set; } = new Reviews();
+	}
+}
---
+++ EmployeeReviews.Services/Reviews.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeReviews.Services
+{
+	class Reviews
+	{
+		public string ReviewNotes { get; set; }
+		public bool Satisfactory { get; set; }
+	}
+}
